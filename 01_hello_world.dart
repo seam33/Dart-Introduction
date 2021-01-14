7be2d30dc6e48ecd3ec15a13e05d255a64d80ac3
seam33 <sebastian3304@gmail.com>
2021-01-14 11:28:55 -0500

    Hello World file was added.

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("Hello world Dart");
+  // ctrl+/ => comment line
+
+  String name = "Sebastian";
+  String name1 = "Sebastian"; // Dynamic
+
+  // $ => Inject a variable
+  print("hello $name");
+  print("hello $name1");
+}
