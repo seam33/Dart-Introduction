0c98b08519ed1d719ed3fd46f86cf56a5a9307da
seam33 <sebastian3304@gmail.com>
2021-01-14 11:29:58 -0500

    Data types file was added.

---
+++ 02_data_types.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Numbers
+  int budget = 15;
+  double budget1 = 3.14;
+  var budget2 = 345.4;
+
+  print('$budget - $budget1 - $budget2');
+
+  // Strings
+  String name = "Sebastian";
+  print(name[0]); // first character
+  print(name[name.length - 1]); // las
+}
